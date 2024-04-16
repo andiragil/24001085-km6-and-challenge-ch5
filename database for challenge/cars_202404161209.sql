7c68cecd53e7aaf5b181ce19ba20fb34773349bf
andragil <andiragilw09@gmail.com>
2024-04-16 12:11:13 +0700

    second

---
+++ database for challenge/cars_202404161209.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.cars (name,image,detail_id,"rentPerDay","size","createdAt","updatedAt","deletedAt") VALUES
+	 ('yaris','https://res.cloudinary.com/dyypdddve/image/upload/v1711693515/796997fc4aa546a15e134aaa8fed3542.jpg',1,190000,'small','2024-03-29 13:25:15.972+07','2024-03-29 13:25:15.972+07',NULL),
+	 ('mustang','https://res.cloudinary.com/dyypdddve/image/upload/v1711785485/3f1f2550d32c276dde3ca73de6c0934f.jpg',1,175000,'medium','2024-03-30 14:58:05.685+07','2024-03-30 14:58:05.685+07',NULL),
+	 ('toyota corola','https://res.cloudinary.com/dyypdddve/image/upload/v1711786634/b779dc29caea0270005a17302d33bf3e.jpg',4,150000,'small','2024-03-27 16:45:25.982+07','2024-03-30 15:17:14.245+07',NULL),
+	 ('suzuki grand vitara','https://res.cloudinary.com/dyypdddve/image/upload/v1711786674/8faa02f28a2f2016bb3d0996de295764.jpg',2,180000,'large','2024-03-27 19:43:26.852+07','2024-03-30 15:17:55.046+07',NULL),
+	 ('mitsubishi colt l300','https://res.cloudinary.com/dyypdddve/image/upload/v1711786728/911d86e06fbba7452e53a9a98f92f8c6.jpg',1,80000,'medium','2024-03-27 17:08:00.235+07','2024-03-30 15:18:48.503+07',NULL),
+	 ('daihatsu xenia','https://res.cloudinary.com/dyypdddve/image/upload/v1711786792/b60af98759b86623925e9274b1116637.jpg',3,170000,'medium','2024-03-27 19:48:11.941+07','2024-03-30 15:19:52.541+07',NULL),
+	 ('mitsubishi pajero sport','https://res.cloudinary.com/dyypdddve/image/upload/v1711787019/76321b116a0e20ec8f98b0c3e6d6f87c.jpg',2,400000,'large','2024-03-27 19:40:37.096+07','2024-03-30 15:23:39.35+07',NULL),
+	 ('honda civic 90','https://res.cloudinary.com/dyypdddve/image/upload/v1711787064/12b64cc76e2cf4a8887875ee3002c35f.jpg',1,900000,'small','2024-03-27 19:42:56.304+07','2024-03-30 15:24:24.688+07',NULL),
+	 ('suzuki ertiga','https://res.cloudinary.com/dyypdddve/image/upload/v1711787369/0e8bf707769ce2e5a6fbe162bba12785.png',3,250000,'medium','2024-03-27 19:47:59.578+07','2024-03-30 15:29:30.12+07',NULL),
+	 ('hyundai palisade','https://res.cloudinary.com/dyypdddve/image/upload/v1713234229/1fe811e314f4f96a02cd29bb7810b332.jpg',2,450000,'large','2024-04-16 09:23:50.288+07','2024-04-16 09:23:50.288+07',NULL);
