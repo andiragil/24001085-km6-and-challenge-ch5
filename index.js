90856792d4c452f4e8cb2195b72e1208883c7456
andragil <andiragilw09@gmail.com>
2024-04-19 19:35:19 +0700

    deploy challenge

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require("./route");
 const fileUpload = require("express-fileupload");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.static("public"));
