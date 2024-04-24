d0849545327e4fb165ac8b1ca09ff1439d0793de
andragil <andiragilw09@gmail.com>
2024-04-24 21:56:38 +0700

    cors2

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(
@@ -17,7 +18,7 @@ app.use(
     })
 );
 app.use("/api", router);
-app.use(cors());
+
 
 
 app.use("*", (req, res) => {
